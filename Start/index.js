d8d5a9c9a9bb5681e11bb7f658ebb4eb2feba81a
Newton <newtonyt04@gmail.com>
2025-09-17 20:52:48 +0600

    run how an backend project in local machine

--- Start/index.js
+++ Start/index.js
@@ -1,1 +1,14 @@
-console.log("hello work")
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/Owner", (req, res) => {
+  res.send("Owner is Newton.");
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Hello i am currently running on port number: ${port}`);
+});
